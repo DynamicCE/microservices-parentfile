20b8447de3db8ccb6a2c1c2208025c6cbb9867b0
Adınız <erkanmurat0632@gmail.com>
2024-11-13 22:11:36 +0300

    feat : AccountsController added

--- section2/accounts/src/main/java/com/erkan/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/erkan/accounts/controller/AccountsController.java
@@ -1,5 +1,34 @@
 package com.erkan.accounts.controller;
 
-public
-class AccountsController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.erkan.accounts.constants.AccountsConstants;
+import com.erkan.accounts.dto.CustomerDto;
+import com.erkan.accounts.dto.ResponseDto;
+import com.erkan.accounts.service.IAccountsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class AccountsController {
+
+    private final IAccountsService iAccountService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+
+        iAccountService.createAccount(customerDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
 }
