a8f4d42b2cd17f8f01cf2e616f2c8534513580ec
Adınız <erkanmurat0632@gmail.com>
2024-11-13 22:16:01 +0300

    feat : derived query method added for customer mobile number search

--- section2/accounts/src/main/java/com/erkan/accounts/repository/CustomerRepository.java
+++ section2/accounts/src/main/java/com/erkan/accounts/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.erkan.accounts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.erkan.accounts.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
