5e5069dff716f566c1d5c0f04e86aecc006669cb
Adınız <erkanmurat0632@gmail.com>
2024-11-14 14:36:21 +0300

    feat : Accounts field added to CustomerDto

--- section2/accounts/src/main/java/com/erkan/accounts/dto/CustomerDto.java
+++ section2/accounts/src/main/java/com/erkan/accounts/dto/CustomerDto.java
@@ -5,7 +5,10 @@ import lombok.Data;
 @Data
 public class CustomerDto {
     private String name;
+
     private String email;
+
     private String mobileNumber;
 
+    private AccountsDto accountsDto;
 }
