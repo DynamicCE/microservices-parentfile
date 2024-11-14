b52b609ec9743223240110c68273a194aa986804
Adınız <erkanmurat0632@gmail.com>
2024-11-14 14:37:28 +0300

    feat : custom query method added

--- section2/accounts/src/main/java/com/erkan/accounts/repository/AccountsRepository.java
+++ section2/accounts/src/main/java/com/erkan/accounts/repository/AccountsRepository.java
@@ -1,5 +1,7 @@
 package com.erkan.accounts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.erkan.accounts.model.Accounts;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
-
+    Optional<Accounts> findByCustomerId(Long customerId);
 }
