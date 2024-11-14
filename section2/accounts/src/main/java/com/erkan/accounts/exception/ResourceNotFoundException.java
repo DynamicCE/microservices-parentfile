1e52a69c016c233ff4a73e77d2aad75aa1e36bf1
Adınız <erkanmurat0632@gmail.com>
2024-11-14 14:37:12 +0300

    feat : enhance exception with field details

--- section2/accounts/src/main/java/com/erkan/accounts/exception/ResourceNotFoundException.java
+++ section2/accounts/src/main/java/com/erkan/accounts/exception/ResourceNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException(String message) {
-        super(message);
+    public ResourceNotFoundException(String message, String fieldName, String fieldValue) {
+        super(String.format("%s not found with %s : '%s'", message, fieldName, fieldValue));
     }
 }
